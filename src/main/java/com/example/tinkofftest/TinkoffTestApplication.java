ff68fd7b84ce0c104dce4c652247bca5b04918e1
Mikhail Leonov <mikhail.leonov62@gmail.com>
2023-03-20 11:07:22 +0300

    Initial Commit

---
+++ src/main/java/com/example/tinkofftest/TinkoffTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinkofftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tinkofftest/TinkoffTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinkofftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
