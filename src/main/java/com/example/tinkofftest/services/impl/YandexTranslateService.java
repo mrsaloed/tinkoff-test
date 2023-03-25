993c57ed019b36adfda61775eb686f95020d5cba
Mikhail Leonov <mikhail.leonov62@gmail.com>
2023-03-25 13:54:04 +0300

    Added tests for MessageServiceImpl YandexTranslateService now throw TranslateServiceException when cant pars parameters

--- src/main/java/com/example/tinkofftest/services/impl/YandexTranslateService.java
+++ src/main/java/com/example/tinkofftest/services/impl/YandexTranslateService.java
@@ -8,9 +8,7 @@ import com.example.tinkofftest.exceptions.TranslateServiceException;
 import com.example.tinkofftest.properties.YandexProperties;
 import com.example.tinkofftest.services.TranslateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +22,7 @@ public class YandexTranslateService implements TranslateService {
     private static final String REGEX_FOR_SPLIT_LANGUAGE_PARAMETERS = "-";
     private static final String REGEX_FOR_SPLIT_WORDS_FROM_MESSAGE = "\\s";
     private static final String AUTHORIZATION_HEADER_KEY = "Authorization";
+    private static final String BAD_TRANSLATE_PARAMETERS_EXCEPTION_MESSAGE = "Bad translate parameters!";
     private final YandexProperties yandexProperties;
 
     @Autowired
@@ -45,12 +44,20 @@ public class YandexTranslateService implements TranslateService {
         return getTranslatedMessageFromYandexTranslatedMessage(yandexTranslatedMessage);
     }
 
-    private String getSourceLanguageCodeFromParams(String parameters) {
-        return parameters.split(REGEX_FOR_SPLIT_LANGUAGE_PARAMETERS)[0];
+    private String getSourceLanguageCodeFromParams(String parameters) throws TranslateServiceException {
+        try {
+            return parameters.split(REGEX_FOR_SPLIT_LANGUAGE_PARAMETERS)[0];
+        } catch (Exception e) {
+            throw new TranslateServiceException(HttpStatus.BAD_REQUEST, BAD_TRANSLATE_PARAMETERS_EXCEPTION_MESSAGE);
+        }
     }
 
-    private String getTargetLanguageCodeFromParams(String parameters) {
-        return parameters.split(REGEX_FOR_SPLIT_LANGUAGE_PARAMETERS)[1];
+    private String getTargetLanguageCodeFromParams(String parameters) throws TranslateServiceException {
+        try{
+            return parameters.split(REGEX_FOR_SPLIT_LANGUAGE_PARAMETERS)[1];
+        } catch (Exception e) {
+            throw new TranslateServiceException(HttpStatus.BAD_REQUEST, BAD_TRANSLATE_PARAMETERS_EXCEPTION_MESSAGE);
+        }
     }
 
     private String[] splitMessageToWords(String message) {
---
+++ src/test/java/com/example/tinkofftest/services/impl/MessageServiceImplTest.java
@@ -0,0 +1,119 @@
+package com.example.tinkofftest.services.impl;
+
+import com.example.tinkofftest.dto.MessageToTranslateBody;
+import com.example.tinkofftest.exceptions.MessageServiceException;
+import com.example.tinkofftest.exceptions.TranslateServiceException;
+import com.example.tinkofftest.services.TranslateService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MessageServiceImplTest {
+
+    private static final String CORRECT_MESSAGE = "Hi, Tinkoff!";
+    private static final String CORRECT_PARAMS = "en-ru";
+    private static final String INCORRECT_PARAMS = "params";
+    private static final String EMPTY_MESSAGE = "";
+    @Autowired
+    private MessageServiceImpl messageService;
+
+    @MockBean
+    private TranslateService translateService;
+
+    private MessageToTranslateBody messageToTranslateBody;
+
+    @BeforeEach
+    void init() {
+        messageToTranslateBody = getCorrectMessage();
+        messageService.translate(messageToTranslateBody);
+        Mockito.doThrow(TranslateServiceException.class)
+                .when(translateService)
+                .translate(EMPTY_MESSAGE, CORRECT_PARAMS);
+        Mockito.doThrow(TranslateServiceException.class)
+                .when(translateService)
+                .translate(CORRECT_MESSAGE, INCORRECT_PARAMS);
+    }
+
+    @Test
+    void shouldReturnCorrectParam() {
+        String actual = messageService.getTranslateParameters();
+        assertEquals(CORRECT_PARAMS, actual);
+    }
+
+    @Test
+    void getTranslateParametersFail() {
+        assertThrows(MessageServiceException.class, () -> {
+            messageToTranslateBody = getIncorrectMessage();
+            messageService.translate(messageToTranslateBody);
+        });
+        assertThrows(MessageServiceException.class, () -> {
+            messageToTranslateBody = getIncorrectParams();
+            messageService.translate(messageToTranslateBody);
+        });
+    }
+
+    @Test
+    void getMessage() {
+        String actual = messageService.getMessage();
+        assertEquals(CORRECT_MESSAGE, actual);
+    }
+
+    @Test
+    void getMessageFail() {
+        assertThrows(MessageServiceException.class, () -> {
+            messageToTranslateBody = getIncorrectMessage();
+            messageService.translate(messageToTranslateBody);
+        });
+        assertThrows(MessageServiceException.class, () -> {
+            messageToTranslateBody = getIncorrectParams();
+            messageService.translate(messageToTranslateBody);
+        });
+    }
+
+    @Test
+    void translate() {
+        Mockito.verify(translateService)
+                .translate(CORRECT_MESSAGE, CORRECT_PARAMS);
+        assertEquals(CORRECT_MESSAGE, messageService.getMessage());
+        assertEquals(CORRECT_PARAMS, messageService.getTranslateParameters());
+    }
+
+    @Test
+    void getFromTranslatedWords() {
+        Mockito.verify(translateService)
+                .translate(CORRECT_MESSAGE, CORRECT_PARAMS);
+    }
+
+    @Test
+    void getRequestEntity() {
+        Mockito.verify(translateService)
+                .translate(CORRECT_MESSAGE, CORRECT_PARAMS);
+    }
+
+    private MessageToTranslateBody getCorrectMessage() {
+        MessageToTranslateBody message = new MessageToTranslateBody();
+        message.setMessage(CORRECT_MESSAGE);
+        message.setParameters(CORRECT_PARAMS);
+        return message;
+    }
+
+    private MessageToTranslateBody getIncorrectParams() {
+        MessageToTranslateBody message = new MessageToTranslateBody();
+        message.setMessage(CORRECT_MESSAGE);
+        message.setParameters(INCORRECT_PARAMS);
+        return message;
+    }
+
+    private MessageToTranslateBody getIncorrectMessage() {
+        MessageToTranslateBody message = new MessageToTranslateBody();
+        message.setMessage(EMPTY_MESSAGE);
+        message.setParameters(CORRECT_PARAMS);
+        return message;
+    }
+}
